3f7aff2c96e7c880fd42d492a569c47ed7b8f700
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-07-22 08:34:49 -0300

    go/weather-forecast solution

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather contains ForestCast.
 package weather
 
+// CurrentCondition holds the current weather.
 var CurrentCondition string
+
+// CurrentLocation holds the location of the weather.
 var CurrentLocation string
 
+// Forecast returns the weather for the city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
