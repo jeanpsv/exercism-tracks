70f066f8495104360c70d5d3f8ed25b5f4820adb
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-08-14 11:20:12 -0300

    add go/lasagna-master solution

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,35 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, averagePreparationTime int) int {
+	if averagePreparationTime == 0 {
+		averagePreparationTime = 2
+	}
+	return len(layers) * averagePreparationTime
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	var noodleLayers int = 0
+	var sauceLayers int = 0
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodleLayers++
+		}
+		if layer == "sauce" {
+			sauceLayers++
+		}
+	}
 
-// TODO: define the 'AddSecretIngredient()' function
+	return noodleLayers * 50, float64(sauceLayers) * 0.2
+}
 
-// TODO: define the 'ScaleRecipe()' function
+func AddSecretIngredient(friendsList []string, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
+}
+
+func ScaleRecipe(quantities []float64, numberOfPortions int) []float64 {
+	var scaledPortions = []float64{}
+	for _, quantity := range quantities {
+		scaledPortions = append(scaledPortions, quantity*float64(numberOfPortions)/2)
+	}
+	return scaledPortions
+}
