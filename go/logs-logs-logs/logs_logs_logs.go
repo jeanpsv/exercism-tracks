59a219f5a7b573b5b945ffd3b68bfc1b39d16fda
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-08-16 21:20:25 -0300

    add go/logs-logs-logs solution

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,30 @@
 package logs
 
+import "strings"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, char := range log {
+		switch char {
+		case '\u2757':
+			return "recommendation"
+		case '\U0001f50d':
+			return "search"
+		case '\u2600':
+			return "weather"
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return len([]rune(log)) <= limit
 }
