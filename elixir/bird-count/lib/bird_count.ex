22b0562b3a2428f7c9323e23376e3f36d6cce6cd
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-09-15 21:34:19 -0300

    add elixir/bird-count solution

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,26 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
+  def today([]), do: nil
+  def today([head | _tail]), do: head
+
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
+
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([head | tail]) do
+    not_visited?(head) || has_day_without_birds?(tail)
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  defp not_visited?(0), do: true
+  defp not_visited?(_), do: false
+
+  def total([]), do: 0
+  def total([head | tail]) do
+    head + total(tail)
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  defp busy?(head) when head >= 5, do: 1
+  defp busy?(_), do: 0
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
-
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def busy_days([]), do: 0
+  def busy_days([head | tail]), do: busy?(head) + busy_days(tail)
 end
